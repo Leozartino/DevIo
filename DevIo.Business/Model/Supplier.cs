137fd0b7dde057eabe2e3c0de65dbf99375a7b7f
Leonardo Oliveira <leozartino@gmail.com>
2024-02-26 10:17:23 +0100

    feat: add supplier model

---
+++ DevIo.Business/Model/Supplier.cs
@@ -0,0 +1,11 @@
+﻿namespace DevIo.Business.Model
+{
+    public class Supplier
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public int SupplierType { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
