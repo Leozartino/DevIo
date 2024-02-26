d0e56db67f07621f4ef89ce85b7d9550727c0e66
Leonardo Oliveira <leozartino@gmail.com>
2024-02-26 10:16:56 +0100

    feat: add AplicationConfig to map appsettings

---
+++ DevIo.Api/Utils/ApplicationSettings/ApplicationConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace DevIo.Api.Utils.ApplicationSettings
+{
+    public class ApplicationConfig
+    {
+        public ConnectionStrings ConnectionStrings { get; set; }
+    }
+
+    public class ConnectionStrings
+    {
+        public string DefaultConnection { get; set; }
+    }
+}
