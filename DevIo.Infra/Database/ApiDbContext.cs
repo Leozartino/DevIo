84b8ca5739d4053f884307effd6c4ebbc0f875df
Leonardo Oliveira <leozartino@gmail.com>
2024-02-26 10:17:11 +0100

    feat: add dbcontext to map suppliers

---
+++ DevIo.Infra/Database/ApiDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DevIo.Business.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace DevIo.Infra.Database
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ DevIo.Infra/Database/Configuration/SupplierConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using DevIo.Business.Model;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIo.Infra.Database.Configuration
+{
+    internal class SupplierConfiguration : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Document).IsRequired().HasMaxLength(14);
+        }
+    }
+}
