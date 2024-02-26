31e1ce3c41739c75627f4b66d6f32c7423814d98
Leonardo Oliveira <leozartino@gmail.com>
2024-02-26 10:17:47 +0100

    feat: add ef config in program and appconfig DI

--- DevIo.Api/Program.cs
+++ DevIo.Api/Program.cs
@@ -1,8 +1,22 @@
+using DevIo.Api.Utils.ApplicationSettings;
+using DevIo.Infra.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.Configure<ApplicationConfig>(configuration);
+var applicationConfig = configuration.Get<ApplicationConfig>();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApiDbContext>(optionsAction: options =>
+{
+    options.UseSqlServer(applicationConfig.ConnectionStrings.DefaultConnection);
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
