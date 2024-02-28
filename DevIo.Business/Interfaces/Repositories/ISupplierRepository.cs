0d685cb3a6cad0bf256b15e978ccdb3c2159915d
Leonardo Oliveira <leozartino@gmail.com>
2024-02-28 20:52:11 +0100

    feat: add supplier interface

---
+++ DevIo.Business/Interfaces/Repositories/ISupplierRepository.cs
@@ -0,0 +1,10 @@
+﻿using DevIo.Business.Model;
+
+namespace DevIo.Business.Interfaces.Repositories
+{
+    interface ISupplierRepository : IRepository<Supplier>
+    {
+        Task<Supplier> GetSupplierWithAddressAsync(Guid id);
+        Task<Supplier> GetSupplierWithAddressAndProductsAsync(Guid id);
+    }
+}
