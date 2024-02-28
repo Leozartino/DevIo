25aa01d3eb4cae161cbadbfcdf4688647bfbb747
Leonardo Oliveira <leozartino@gmail.com>
2024-02-28 20:47:35 +0100

    feat: add IRepository interface

---
+++ DevIo.Business/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevIo.Business.Model;
+using System.Linq.Expressions;
+
+namespace DevIo.Business.Interfaces.Repositories
+{
+    interface IRepository<TEntity> : IDisposable where TEntity : BaseEntity
+    {
+        Task Add(TEntity entity);
+        Task<TEntity> GetById(Guid id);
+        Task<List<TEntity>> GetAll();
+        Task Update(TEntity entity);
+        Task Remove(Guid id);
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
