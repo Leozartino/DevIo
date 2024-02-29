812f772b536f95e97bcf3582fef82388a9e5d0d3
Leonardo Oliveira <leozartino@gmail.com>
2024-02-29 22:44:40 +0100

    refact: minor change to break line in constructor method

--- DevIo.Api/Exceptions/MissingConfigurationException.cs
+++ DevIo.Api/Exceptions/MissingConfigurationException.cs
@@ -4,7 +4,8 @@
     {
         public string Key { get; private set; }
 
-        public MissingConfigurationException(string key) : base($"Missing configuration value for key: '{key}'")
+        public MissingConfigurationException(string key) 
+            : base($"Missing configuration value for key: '{key}'")
         {
             Key = key;
         }
