60a50a7e1bd442c9dab9934067627171c16dcd09
Leonardo Oliveira <leozartino@gmail.com>
2024-02-29 22:41:47 +0100

    feat: add missingconfigurationexeception for appsettings

---
+++ DevIo.Api/Exceptions/MissingConfigurationException.cs
@@ -0,0 +1,12 @@
+﻿namespace DevIo.Api.Exceptions
+{
+    public class MissingConfigurationException : Exception
+    {
+        public string Key { get; private set; }
+
+        public MissingConfigurationException(string key) : base($"Missing configuration value for key: '{key}'")
+        {
+            Key = key;
+        }
+    }
+}
