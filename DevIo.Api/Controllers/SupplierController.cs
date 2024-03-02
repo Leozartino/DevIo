ff5003cf450d943a0ab97cd09e68d751694759fe
Leonardo Oliveira <leozartino@gmail.com>
2024-03-02 11:48:18 +0100

    feat: add supplier getall endpoint

--- DevIo.Api/Controllers/SupplierController.cs
+++ DevIo.Api/Controllers/SupplierController.cs
@@ -1,16 +1,71 @@
 ﻿using DevIo.Api.Dtos;
+using DevIo.Business.Interfaces.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevIo.Api.Controllers
 {
     public class SupplierController : MainController
     {
+
+        private readonly ISupplierRepository _supplierRepository;
+
+        public SupplierController(ISupplierRepository supplierRepository)
+        {
+            _supplierRepository = supplierRepository;
+        }
+
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<SupplierDto>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<IEnumerable<SupplierDto>>> GetSuppliers()
         {
-            return Ok();
+            var suppliers = await _supplierRepository.GetAll();
+
+            if (suppliers.Count == 0)
+            { 
+                return Ok(suppliers);
+            }
+
+            var suppliersDto = suppliers.Select(supplier =>
+            {
+                return new SupplierDto
+                {
+                    Id = supplier.Id,
+                    Name = supplier.Name,
+                    Document = supplier.Document,
+                    SupplierType = supplier.SupplierType,
+                    IsActive = supplier.IsActive,
+                    Address = new AddressDto
+                    {
+                        Id = supplier.Address.Id,
+                        Street = supplier.Address.Street,
+                        Number = supplier.Address.Number,
+                        Complement = supplier.Address.Complement,
+                        PostalCode = supplier.Address.PostalCode,
+                        Neighbourhood = supplier.Address.Neighbourhood,
+                        Municipality = supplier.Address.Municipality,
+                        AdministrativeArea = supplier.Address.AdministrativeArea,
+                    },
+
+                    Products = supplier.Products.Select(product =>
+                    {
+                        return new ProductDto
+                        {
+                            Id = product.Id,
+                            Name = product.Name,
+                            Description = product.Description,
+                            Image = product.Image,
+                            ImageUpload = "S3 URL",
+                            IsActive = product.IsActive,
+                            //SupplierName = supplier.Name,
+                            Value = product.Value,
+                            CreatedAt = product.CreatedAt,
+                        };
+                    }),
+                };
+            });
+
+            return Ok(suppliersDto);
         }
     }
 }
