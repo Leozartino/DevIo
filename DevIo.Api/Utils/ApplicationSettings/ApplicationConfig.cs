18d0375b5f192c389b6559c90c0aa96315082490
Leonardo Oliveira <leozartino@gmail.com>
2024-03-02 10:38:27 +0100

    refact: remove code validation from applicationconfig

--- DevIo.Api/Utils/ApplicationSettings/ApplicationConfig.cs
+++ DevIo.Api/Utils/ApplicationSettings/ApplicationConfig.cs
@@ -4,44 +4,14 @@ namespace DevIo.Api.Utils.ApplicationSettings
 {
     public class ApplicationConfig
     {
-        private ConnectionStrings? _connectionStrings { get; set; }
+        public ConnectionStrings? ConnectionStrings { get; set; }
 
-        public ConnectionStrings ConnectionStrings 
-        { 
-            get 
-            { 
-                if(_connectionStrings == null)
-                {
-                    throw new MissingConfigurationException("ConnectionStrings");
-                }
-                return _connectionStrings;
-            }
-            set 
-            { 
-                _connectionStrings = value; 
-            }
-        }
 
     }
 
     public class ConnectionStrings
     {
-        private string? _defaultConnection { get; set; }
+        public string? DefaultConnection { get; set; }
 
-        public string DefaultConnection 
-        { 
-            get
-            {
-                if(_defaultConnection == null)
-                {
-                    throw new MissingConfigurationException("ConnectionStrings:DefaultConnection");
-                }
-                return _defaultConnection;
-            }
-            set
-            {
-                _defaultConnection = value;
-            }
-        }
     }
 }
