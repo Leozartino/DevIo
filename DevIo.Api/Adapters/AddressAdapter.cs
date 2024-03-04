af16734edb8c49d514a787ad9d5c8f592fdca4bd
Leonardo Oliveira <leozartino@gmail.com>
2024-03-04 23:09:48 +0100

    feat: add address and supplier adapter

---
+++ DevIo.Api/Adapters/AddressAdapter.cs
@@ -0,0 +1,24 @@
+﻿using DevIo.Api.Dtos.Request;
+using DevIo.Business.Interfaces;
+using DevIo.Business.Model;
+
+namespace DevIo.Api.Adapters
+{
+    public class AddressAdapter : IAdapter<AddressRequestDto, Address>
+    {
+        public Address ConvertToDestinationObject(AddressRequestDto source)
+        {
+            return new Address
+            {
+                Street = source.Street,
+                SupplierId = source.SupplierId,
+                PostalCode = source.PostalCode,
+                Number = source.Number,
+                Neighbourhood = source.Neighbourhood,
+                Municipality = source.Municipality,
+                AdministrativeArea = source.AdministrativeArea,
+                Complement = source.Complement            
+            };
+        }
+    }
+}
---
+++ DevIo.Api/Adapters/SupplierAdapter.cs
@@ -0,0 +1,39 @@
+﻿using DevIo.Api.Dtos.Request;
+using DevIo.Business.Interfaces;
+using DevIo.Business.Model;
+
+namespace DevIo.Api.Adapters
+{
+    public class SupplierAdapter : IAdapter<SupplierRequestDto, Supplier>
+    {
+        private Supplier supplier = new Supplier();
+
+        public Supplier ConvertToDestinationObject(SupplierRequestDto source)
+        {
+
+            supplier.Name = source.Name;
+            supplier.Document = source.Document;
+            supplier.IsActive = source.IsActive;
+            supplier.SupplierType = source.SupplierType;
+            supplier.Address = ConvertToDestinationObject(source.Address);
+            
+            return supplier;
+
+        }
+
+        private Address ConvertToDestinationObject(AddressRequestDto source)
+        {
+            return new Address
+            {
+                Street = source.Street,
+                SupplierId = supplier.Id,
+                PostalCode = source.PostalCode,
+                Number = source.Number,
+                Neighbourhood = source.Neighbourhood,
+                Municipality = source.Municipality,
+                AdministrativeArea = source.AdministrativeArea,
+                Complement = source.Complement
+            };
+        }
+    }
+}
