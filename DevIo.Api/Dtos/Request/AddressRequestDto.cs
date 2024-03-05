087760b8b0d92b35038270f6f3969654de570ddf
Leonardo Oliveira <leozartino@gmail.com>
2024-03-05 12:19:40 +0100

    feat: add addressequestdto

--- DevIo.Api/Dtos/Request/AddressRequestDto.cs
+++ DevIo.Api/Dtos/Request/AddressRequestDto.cs
@@ -1,12 +1,7 @@
-﻿using System.Text.Json.Serialization;
-
-namespace DevIo.Api.Dtos.Request
+﻿namespace DevIo.Api.Dtos.Request
 {
     public class AddressRequestDto
     {
-
-        [JsonIgnore] 
-        public Guid SupplierId { get; set; }
         public string Street { get; set; }
         public string Number { get; set; }
         public string Complement { get; set; }
@@ -14,5 +9,6 @@ namespace DevIo.Api.Dtos.Request
         public string Neighbourhood { get; set; }
         public string Municipality { get; set; }
         public string AdministrativeArea { get; set; }
+
     }
 }
