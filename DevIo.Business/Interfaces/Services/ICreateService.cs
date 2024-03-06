398f8796353dff917556254844dc3a88e8a1f391
Leonardo Oliveira <leozartino@gmail.com>
2024-03-06 21:45:12 +0100

    feat: add services layer and supplier service

---
+++ DevIo.Business/Interfaces/Services/ICreateService.cs
@@ -0,0 +1,7 @@
+﻿namespace DevIo.Business.Interfaces.Services
+{
+    public interface ICreateService<TCreateDto, TCreateResponse>
+    {
+        Task<TCreateResponse> CreateAsync(TCreateDto createDto);
+    }
+}
