c37cf4d43b20f6664cd79e3fa4c5fb4b8823ebfd
Leonardo Oliveira <leozartino@gmail.com>
2024-03-07 13:52:11 +0100

    feat: add validator for supplier create dto

---
+++ DevIo.Api/Validators/AddressCreateDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using DevIo.Api.Dtos.Request;
+using FluentValidation;
+
+namespace DevIo.Api.Validators
+{
+    public class AddressCreateDtoValidator : AbstractValidator<AddressCreateDto>
+    {
+        public AddressCreateDtoValidator()
+        {
+            RuleFor(dto => dto.Street).NotEmpty();
+            RuleFor(dto => dto.Number).NotEmpty();
+            RuleFor(dto => dto.PostalCode).NotEmpty();
+            RuleFor(dto => dto.Neighbourhood).NotEmpty();
+            RuleFor(dto => dto.Municipality).NotEmpty();
+            RuleFor(dto => dto.AdministrativeArea).NotEmpty();
+        }
+    }
+}
---
+++ DevIo.Api/Validators/SupplierCreateDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using DevIo.Api.Dtos.Request;
+using FluentValidation;
+
+namespace DevIo.Api.Validators
+{
+    public class SupplierCreateDtoValidator : AbstractValidator<SupplierCreateDto>
+    {
+        public SupplierCreateDtoValidator() 
+        {
+            RuleFor(dto => dto.Name)
+                .NotEmpty()
+                .MaximumLength(200);
+            RuleFor(dto => dto.Document)
+                .NotEmpty()
+                .Must(BeValidBrazilianDocument)
+                .WithMessage("Invalid CPF or CNPJ");
+            RuleFor(dto => dto.SupplierType)
+                .IsInEnum();
+            RuleFor(dto => dto.Address)
+                .SetValidator(new AddressCreateDtoValidator());
+        }
+
+        private bool BeValidBrazilianDocument(string document)
+        {
+            return !string.IsNullOrWhiteSpace(document) && document.Length >= 11;
+        }
+    }
+}
