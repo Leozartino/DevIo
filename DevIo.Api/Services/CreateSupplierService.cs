2d8e5b12fa1a3dfadc04be3b9c209b6a6d9c4b0e
Leonardo Oliveira <leozartino@gmail.com>
2024-03-07 13:54:03 +0100

    refact: reanme create dto

--- DevIo.Api/Services/CreateSupplierService.cs
+++ DevIo.Api/Services/CreateSupplierService.cs
@@ -3,24 +3,37 @@ using DevIo.Business.Interfaces;
 using DevIo.Business.Interfaces.Repositories;
 using DevIo.Business.Interfaces.Services;
 using DevIo.Business.Model;
+using FluentValidation;
 
 namespace DevIo.Services.Services
 {
-    public class CreateSupplierService : ICreateService<SupplierRequestDto, Supplier>
+    public class CreateSupplierService : ICreateService<SupplierCreateDto, Supplier>
     {
-        private readonly IAdapter<SupplierRequestDto, Supplier> _adapter;
+        private readonly IAdapter<SupplierCreateDto, Supplier> _adapter;
         private readonly ISupplierRepository _supplierRepository;
+        private readonly IValidator<SupplierCreateDto> _validator;
 
-        public CreateSupplierService(
-            IAdapter<SupplierRequestDto, Supplier> adapter,
-            ISupplierRepository supplierRepository)
+        public CreateSupplierService
+            (
+            IAdapter<SupplierCreateDto, Supplier> adapter, 
+            ISupplierRepository supplierRepository, 
+            IValidator<SupplierCreateDto> validator
+            )
         {
             _adapter = adapter;
             _supplierRepository = supplierRepository;
+            _validator = validator;
         }
 
-        public async Task<Supplier> CreateAsync(SupplierRequestDto createDto)
+        public async Task<Supplier> CreateAsync(SupplierCreateDto createDto)
         {
+            var validationResult = await _validator.ValidateAsync(createDto);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+
             try
             {
                 Supplier supplier = _adapter.ConvertToDestinationObject(createDto);
