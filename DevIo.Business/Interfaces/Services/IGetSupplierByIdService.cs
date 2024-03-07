cbc831264836fd3961842518fac2a36f38f56e1c
Leonardo Oliveira <leozartino@gmail.com>
2024-03-07 13:54:46 +0100

    feat: add interface for IgetsupplierbyIdservice

---
+++ DevIo.Business/Interfaces/Services/IGetSupplierByIdService.cs
@@ -0,0 +1,7 @@
+﻿namespace DevIo.Business.Interfaces.Services
+{
+    public interface IGetSupplierByIdService<TResponse> where TResponse : class
+    {
+        Task<TResponse> GetSupplierById(Guid id);
+    }
+}
