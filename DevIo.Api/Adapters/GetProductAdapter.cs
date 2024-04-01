cbaf2bf45da997fde26268e0409af0b068dba338
Leonardo Oliveira <leozartino@gmail.com>
2024-04-01 13:14:01 +0200

    feat: add GetProductAdapter.cs

---
+++ DevIo.Api/Adapters/GetProductAdapter.cs
@@ -0,0 +1,24 @@
+﻿using DevIo.Api.Dtos;
+using DevIo.Business.Interfaces;
+using DevIo.Business.Model;
+
+namespace DevIo.Api.Adapters
+{
+    public class GetProductAdapter : IAdapter<Product, ProductDto>
+    {
+        public ProductDto ConvertToDestinationObject(Product source)
+        {
+            return new ProductDto
+            {
+                Id = source.Id,
+                Name = source.Name,
+                Description = source.Description,
+                IsActive = source.IsActive,
+                ImageUpload = source.Image,
+                Value = source.Value,
+                SupplierId = source.SupplierId,
+                CreatedAt = source.CreatedAt,
+            };
+        }
+    }
+}
